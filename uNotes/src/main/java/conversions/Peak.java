3bf5c79e891f956423d9be35941d504bbfeadb64
PolyakovD <polyakovdmi93gmail.com>
2014-10-18 19:37:27 +0400

    PeakExtracter

---
+++ uNotes/src/main/java/conversions/Peak.java
@@ -0,0 +1,15 @@
+package сonversions;
+
+public class Peak{
+    public double power;
+    public double powerRel;
+    public double center;
+    public double width;
+
+    public Peak(double pow, double powNoise, double center, double width){
+        this.power = pow;
+        this.powerRel = pow - powNoise;
+        this.center = center;
+        this.width = width;
+    }
+}
---
+++ uNotes/src/main/java/conversions/PeakExtracter.java
@@ -0,0 +1,65 @@
+package сonversions;
+
+import java.util.Vector;
+
+public class PeakExtracter{
+
+    private Vector<double[]> raws;
+    private Vector<Vector<Peak>> peaks;
+    private double timeStep;
+    private double freqStep;
+
+    public PeakExtracter(double timeStep, double freqStep){
+        this.timeStep = timeStep;
+        this.freqStep = freqStep;
+    }
+
+    public void loadRaws(Vector<double[]> raws){
+        this.raws = raws;
+    }
+
+    public void extract(){
+        int fsize = raws.get(0).length;
+        int tsize = raws.size();
+        int decline = 0;
+        int declineNew = 0;
+        double c_freq = 0;
+        double l_freq = 0;
+        double c_pow = 0;
+        double l_pow = 0;
+        peaks = new Vector<Vector<Peak>>();
+        for (int i = 0; i < tsize; ++i){
+            double[] cur = raws.get(i);
+            l_freq = 0;
+            l_pow = cur[0];
+            decline = 0;
+            Vector<Peak> result_cur = new Vector<Peak>();
+            peaks.add(result_cur);
+            for (int j = 1; j < fsize - 1; ++j) {
+                if ((cur[j - 1] <= cur[j]) && (cur[j + 1] <= cur[j])) {
+                    declineNew = 1;
+                }
+                if ((cur[j - 1] >= cur[j]) && (cur[j + 1] >= cur[j])) {
+                    declineNew = 0;
+                }
+                if (decline != declineNew) {
+                    if (declineNew == 1) {
+                        c_freq = freqStep * j;
+                        c_pow = cur[j];
+                    } else {
+                        double noise = (l_pow + cur[j]) * 0.5;
+                        result_cur.add(new Peak(c_pow, noise, c_freq, freqStep * j - l_freq));
+                        l_freq = freqStep * j;
+                        l_pow = cur[j];
+                    }
+                    decline = declineNew;
+                }
+            }
+        }
+    }
+
+    public Vector<Vector<Peak>> getPeaks(){
+        return peaks;
+    }
+
+}
