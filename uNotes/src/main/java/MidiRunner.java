f995706b2f579ada7644561e635f3ed5e14fddd5
Vasiliy Dommes <vasdommes@gmail.com>
2014-11-12 17:03:35 +0300

    NoteSequence class added
    NoteSequence class allows to read notes sequence from MIDI file and to
    get currently plating notes at given moment.
    
    MidiRunner tests this functionality

---
+++ uNotes/src/main/java/MidiRunner.java
@@ -0,0 +1,49 @@
+import conversions.notes.NoteSequence;
+
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+/**
+ * Test for NoteSequence.getMidiSequence()
+ *
+ * @see conversions.notes.NoteSequence
+ */
+public class MidiRunner {
+    public static void main(String[] args) throws FileNotFoundException {
+        File inputDir = new File("test", "music");
+        File outputDir = new File("test", "output");
+        outputDir.mkdir();
+
+        String inputFileName = "a.mid";
+        File in = new File(inputDir, inputFileName);
+
+        System.out.println("uNotes");
+
+        try {
+            NoteSequence noteSequence = new NoteSequence(in);
+            Sequence midiSequence = noteSequence.getMidiSequence();
+
+            File outMidi = new File(outputDir, inputFileName);
+            MidiSystem.write(midiSequence, 0, outMidi);
+
+            ArrayList<double[]> notePower = noteSequence.getNotesSeries(8000);
+            PrintStream outPower = new PrintStream(new File(outputDir, inputFileName + ".npw.dat"));
+
+            for (int i = 0; i < notePower.size(); ++i) {
+                outPower.print(midiSequence.getMicrosecondLength() * 1e-6 * i / notePower.size() + " ");
+                for (int j = 0; j < notePower.get(i).length; j++) {
+                    outPower.print(notePower.get(i)[j] + " ");
+                }
+                outPower.println();
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ uNotes/src/main/java/conversions/notes/NoteSequence.java
@@ -0,0 +1,91 @@
+package conversions.notes;
+
+import com.sun.istack.internal.NotNull;
+
+import javax.sound.midi.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class NoteSequence {
+    private Sequence mySequence;
+    private Track myTrack;
+
+    long myLength;  //Length, ticks
+    double myDuration;  // Duration, seconds
+
+    public NoteSequence(@NotNull File midiInputFile) throws InvalidMidiDataException, IOException {
+        mySequence = MidiSystem.getSequence(midiInputFile);
+        myLength = mySequence.getTickLength();
+        if (mySequence.getMicrosecondLength() == MidiFileFormat.UNKNOWN_LENGTH) {
+            throw new InvalidMidiDataException("Unknown length");
+        }
+        myDuration = mySequence.getMicrosecondLength() / 1.0e6;
+        Track[] tracks = mySequence.getTracks();
+
+        // Move all notes in single track
+        // Catch only NOTE_ON and NOTE_OFF events
+        //myTrack = tracks[0];
+        myTrack = mySequence.createTrack();
+        for (Track track : tracks) {
+            for (int i = 0; i < track.size(); i++) {
+                MidiEvent event = track.get(i);
+                myTrack.add(event);
+                MidiMessage midiMessage = event.getMessage();
+                if (midiMessage instanceof ShortMessage) {
+                    ShortMessage message = (ShortMessage) midiMessage;
+                    int command = message.getCommand();
+                    int key = message.getData1();
+                    int velocity = message.getData2();
+
+                    if (command == ShortMessage.NOTE_ON && velocity == 0) { //Equivalent to NOTE_OFF
+                        myTrack.add(new MidiEvent(new ShortMessage(ShortMessage.NOTE_OFF, key, 127), event.getTick()));
+                    } else if (command == ShortMessage.NOTE_ON || command == ShortMessage.NOTE_OFF) {
+                        myTrack.add(event);
+                    }
+                }
+            }
+            // delete this track from the sequence
+            mySequence.deleteTrack(track);
+        }
+    }
+
+    public Sequence getMidiSequence() {
+        return mySequence;
+    }
+
+    public ArrayList<double[]> getNotesSeries(int outputArrayLength) {
+        ArrayList<double[]> result = new ArrayList<double[]>();
+
+        double step = 1.0 * outputArrayLength / myTrack.ticks();
+        HashSet<Integer> currentNotes = new HashSet<Integer>();
+
+
+        int prevIndex = 0;
+        for (int i = 0; i < myTrack.size(); i++) {
+            MidiEvent event = myTrack.get(i);
+            int currIndex = (new Double(event.getTick() * step)).intValue();
+            for (int j = prevIndex; j < currIndex; j++) {
+                double[] t = new double[128];
+                for (Integer note : currentNotes) {
+                    t[note] = 1;    //Mark note as playing
+                }
+                result.add(t);
+            }
+            prevIndex = currIndex;
+
+            if (event.getMessage() instanceof ShortMessage) {
+                ShortMessage message = (ShortMessage) event.getMessage();
+                if (message.getCommand() == ShortMessage.NOTE_ON) {
+                    currentNotes.add(message.getData1());
+                } else if (message.getCommand() == ShortMessage.NOTE_OFF) {
+                    currentNotes.remove(message.getData1());
+                }
+            }
+        }
+
+        return result;
+    }
+}
