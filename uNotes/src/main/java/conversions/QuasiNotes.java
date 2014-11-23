df49a973e2e9a79d50f34d90e3d25f99ca85fed6
PolyakovD <polyakovdmi93gmail.com>
2014-11-23 17:42:45 +0300

    Correct criterion.

--- uNotes/src/main/java/conversions/QuasiNotes.java
+++ uNotes/src/main/java/conversions/QuasiNotes.java
@@ -5,27 +5,29 @@ import conversions.notes.NoteAlphabet;
 import conversions.peaks.Peak;
 import conversions.peaks.PeakExtractor;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class QuasiNotes {
-    public static final int relativePowerThreshold = 10;
-    public static final int powerThreshold = 10;
-    public static final double statisticalSignificance = 0.1;
+    public static final int relativePowerThreshold = 20;
+    public static final int powerThreshold = 0;
+    public static final double statisticalSignificance = 0.003;
     private int myMinMidiCode;
     private int myMaxMidiCode;
     private double myTimeStep;
     private ArrayList<double[]> myNotePowerSeries = new ArrayList<double[]>();
 
-    public QuasiNotes(Spectrum spectrum) {
+    public QuasiNotes(Spectrum spectrum) throws FileNotFoundException {
         this(spectrum, MidiHelper.MIN_MIDI_CODE, MidiHelper.MAX_MIDI_CODE);   //all MIDI notes (from C-1 to G9)
     }
 
-    public QuasiNotes(Spectrum spectrum, int minMidiCode, int maxMidiCode) {
+    public QuasiNotes(Spectrum spectrum, int minMidiCode, int maxMidiCode) throws FileNotFoundException {
         myMinMidiCode = minMidiCode;
         myMaxMidiCode = maxMidiCode;
         myTimeStep = spectrum.getTimeStep();
 
         ArrayList<double[]> power = spectrum.getPowerSpectrum();
+        ArrayList<double[]> alignedPower = spectrum.getAlignedPowerSpectrum();
 
         double dt = spectrum.getTimeStep();
         double dnu = spectrum.getFrequencyStep();
@@ -34,7 +36,7 @@ public class QuasiNotes {
         ArrayList<Double> notes = noteAlphabet.getFrequencies();
 
         WaveletSpectrumTransform noteGetter = new WaveletSpectrumTransform(spectrum, noteAlphabet.getAllFrequencies());
-        ArrayList<double[]> wPower = noteGetter.spectrumTransformWithCounts(spectrum);
+        ArrayList<double[]> wPower = noteGetter.spectrumTransformWithCounts(alignedPower);
 
         PeakExtractor pex = new PeakExtractor(dt, dnu);
         pex.loadSpectrum(power);
@@ -46,9 +48,33 @@ public class QuasiNotes {
             myNotePowerSeries.add(new double[notes.size()]);
         }
 
+        //
+        //PrintStream outNotes = new PrintStream(new File(new File("test", "output"), statisticalSignificance + "logger.dat"));
+        //
+        double criticalNoise = 0;
+        for (int i = 0; i < peaks.size(); ++i) {
+            criticalNoise += getCriticalNoise(alignedPower.get(i), statisticalSignificance);
+        }
+        criticalNoise /= peaks.size();
+
         for (int i = 0; i < peaks.size(); ++i) {
             double[] notePowerSlice = new double[notes.size()];
-            double criticalNoise = getCriticalNoise(wPower.get(i), statisticalSignificance);
+            //double criticalNoise = getCriticalNoise(alignedPower.get(i), statisticalSignificance);
+
+            /*
+            double mean = 0;
+            for (int j = 0; j < power.get(i).length; ++j) {
+                mean += power.get(i)[j];
+            }
+            mean /= power.get(i).length;
+            double var = 0;
+            for (int j = 0; j < power.get(i).length; ++j) {
+                var += Math.pow(power.get(i)[j] - mean, 2.0);
+            }
+            var /= (power.get(i).length - 1);
+            outNotes.println(myNotePowerSeries.size() * dt + "   " + criticalNoise + "  " + mean + "  " + Math.sqrt(var));
+            */
+
             for (int j = 0; j < peaks.get(i).size(); j++) {
                 Peak cur = peaks.get(i).get(j);
                 if (cur.powerRel > relativePowerThreshold & cur.power > powerThreshold) {
@@ -88,16 +114,17 @@ public class QuasiNotes {
         return myNotePowerSeries;
     }
 
-    private double getCriticalNoise(double[] selection, double statisticalSignificance) {
+    public static double getCriticalNoise(double[] selection, double statisticalSignificance) {
         if ((statisticalSignificance > 1.0) || (statisticalSignificance < 0.0)) {
             throw new IllegalArgumentException("statisticalSignificance must belong to the interval [0, 1]");
         }
+
         double variance = 0;
         for (int i = 0; i < selection.length; ++i) {
             variance += (selection[i] * selection[i]);
         }
         variance = variance / (double) (selection.length - 1);
 
-        return (-variance * variance * Math.log(statisticalSignificance) / (double) selection.length);
+        return (-variance * Math.log(statisticalSignificance) / (double) selection.length);
     }
 }
